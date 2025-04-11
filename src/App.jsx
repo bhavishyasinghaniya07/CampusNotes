6344ac2e7bfdfdffffc3068a2a6646606c6eb164
Bhavishya Parmar <parmarbhavishya@gmail.com>
2025-04-11 15:25:11 +0530

    added pages using react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./Pages/Home";
+import Signin from "./Pages/Signin";
+import Profile from "./Pages/Profile";
+import Signout from "./Pages/Signup.jsx";
+import About from "./Pages/About";
+import Signup from "./Pages/Signup.jsx";
 
 const App = () => {
-  return <div className="text-red-900 text-3xl font-bold underline">App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/profile" element={<Profile />}></Route>
+        <Route path="/sign-in" element={<Signin />}></Route>
+        <Route path="/sign-up" element={<Signup />}></Route>
+        <Route path="/about" element={<About />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
